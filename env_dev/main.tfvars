7a361100fe52177c9616bc2f71beb12c4c711e78
veeranki2014 <veeranki2014@gmail.com>
2023-08-26 17:07:45 -0500

    aws-terraform

--- env_dev/main.tfvars
+++ env_dev/main.tfvars
@@ -1,1 +1,16 @@
 env = "dev"
+
+components = {
+
+  frontend        = {}
+  mongodb         = {}
+  catalogue       = {}
+  redis           = {}
+  user            = {}
+  cart            = {}
+  mysql           = {}
+  shipping        = {}
+  rabbitmq        = {}
+  payment         = {}
+
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-module "main" {
-  source = "git::https://github.com/veeranki2014/tf_module_app.git"
-  env = var.env
+module "instances" {
+  for_each          = var.components
+  source            = "git::https://github.com/veeranki2014/tf_module_app.git"
+  component         = each.key
+  env               = var.env
+
+
 }
