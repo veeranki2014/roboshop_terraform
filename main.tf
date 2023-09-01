17fd9242046d11d810a579d2d41c8c9bbf8d1c88
veeranki2014 <veeranki2014@gmail.com>
2023-09-01 13:00:35 -0500

    aws-terraform

--- main.tf
+++ main.tf
@@ -3,6 +3,5 @@ module "instances" {
   source            = "git::https://github.com/veeranki2014/tf_module_app.git"
   component         = each.key
   env               = var.env
-
-
+  tags              = each.value["tags"]
 }
