4fa70e54b18e03c9e8980808261dd99e4b0cc2ce
veeranki2014 <veeranki2014@gmail.com>
2023-09-10 16:57:50 -0500

    aws-terraform

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc"{
   source                      = "git::https://github.com/veeranki2014/tf-module-vpc.git"
   for_each                     = var.vpc
   cidr_block                  = each.value["cidr_block"]
-  #subnets                     = each.value["subnets"]
+  subnets                     = each.value["subnets"]
 
   env               = var.env
   tags              = var.tags
