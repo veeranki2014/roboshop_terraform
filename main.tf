b190ba1c320cc095cc2b5c7a366770feff27db83
veeranki2014 <veeranki2014@gmail.com>
2023-09-26 17:19:01 -0500

    terraform rabbitmq module

--- main.tf
+++ main.tf
@@ -28,43 +28,43 @@ module "app_server" {
   subnet_id                   = lookup(lookup(lookup(lookup( module.vpc, "main", null ), "subnet_ids" , null), "app", null), "subnet_ids", null)[0]
 }
 
-module "rabbitmq"{
-  source        = "git::https://github.com/veeranki2014/tf-module-rabbitmq.git"
-  for_each      = var.rabbitmq
-  component     = each.value["component"]
-  instance_type = each.value["instance_type"]
-  env           = var.env
-  tags          = var.tags
-  vpc_id        = lookup(lookup(module.vpc, "main", null ), "vpc_id", null)
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null),"cidr_block",null)
-  subnet_id    = lookup(lookup(lookup(lookup( module.vpc, "main", null ), "subnet_ids" , null), "db", null), "subnet_ids", null)[0]
-  allow_ssh_cidr = var.allow_ssh_cidr
-  zone_id         = var.zone_id
-  kms_key_id      = var.kms_key_id
-}
+#module "rabbitmq"{
+#  source        = "git::https://github.com/veeranki2014/tf-module-rabbitmq.git"
+#  for_each      = var.rabbitmq
+#  component     = each.value["component"]
+#  instance_type = each.value["instance_type"]
+#  env           = var.env
+#  tags          = var.tags
+#  vpc_id        = lookup(lookup(module.vpc, "main", null ), "vpc_id", null)
+#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null),"cidr_block",null)
+#  subnet_id    = lookup(lookup(lookup(lookup( module.vpc, "main", null ), "subnet_ids" , null), "db", null), "subnet_ids", null)[0]
+#  allow_ssh_cidr = var.allow_ssh_cidr
+#  zone_id         = var.zone_id
+#  kms_key_id      = var.kms_key_id
+#}
 
-module "rds" {
-  source    = "git::https://github.com/veeranki2014/tf-module-rds.git"
-
-  for_each            = var.rds
-  component           = each.value["component"]
-  engine              = each.value["engine"]
-  engine_version      = each.value["engine_version"]
-  db_name             = each.value["db_name"]
-  instance_count      = each.value["instance_count"]
-  instance_class      = each.value["instance_class"]
-  subnet_ids          = lookup(lookup(lookup(lookup( module.vpc, "main", null ), "subnet_ids" , null), "db", null), "subnet_ids", null)
-  vpc_id        = lookup(lookup(module.vpc, "main", null ), "vpc_id", null)
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null),"cidr_block",null)
-
-
-  env                 = var.env
-  tags                = var.tags
-  #kms_key_id          = var.kms_key_id
-  kms_key_arn          = var.kms_key_arn
-
-
-}
+#module "rds" {
+#  source    = "git::https://github.com/veeranki2014/tf-module-rds.git"
+#
+#  for_each            = var.rds
+#  component           = each.value["component"]
+#  engine              = each.value["engine"]
+#  engine_version      = each.value["engine_version"]
+#  db_name             = each.value["db_name"]
+#  instance_count      = each.value["instance_count"]
+#  instance_class      = each.value["instance_class"]
+#  subnet_ids          = lookup(lookup(lookup(lookup( module.vpc, "main", null ), "subnet_ids" , null), "db", null), "subnet_ids", null)
+#  vpc_id        = lookup(lookup(module.vpc, "main", null ), "vpc_id", null)
+#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null),"cidr_block",null)
+#
+#
+#  env                 = var.env
+#  tags                = var.tags
+#  #kms_key_id          = var.kms_key_id
+#  kms_key_arn          = var.kms_key_arn
+#
+#
+#}
 
 module "documentdb" {
   source    = "git::https://github.com/veeranki2014/tf-module-documentdb.git"
